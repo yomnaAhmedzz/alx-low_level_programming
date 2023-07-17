664babbe266c5c1a0f2d058bb6ac177e9df9637d
root <ibrahimyomna376@gmail.com>
2023-07-17 16:00:57 -0700

    rtyuiso

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,23 @@
-#include <stdio.hi>
+#include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 /**
  * main - Entry point
+ *
  * Return: Always 0 (Success)
 */
 int main(void)
 {
-char low, e, q;
-e = 'e';
-q = 'q';
-for (low = 'a'; low <= 'z'; low++)
-{
-if (low != e && low != q)
-putchar(low);
-}
-putchar('\n');
-return (0);
+	char n = 'a';
+
+	while (n <= 'z')
+	{
+		if (n != 'e' && n != 'q')
+		{
+			putchar(n);
+		}
+		n++;
+	}
+	putchar ('\n');
+	return (0);
 }
