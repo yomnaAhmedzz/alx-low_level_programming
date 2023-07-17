dba6e8ff214b6ac28bb0fd71c78c6709489212c2
root <ibrahimyomna376@gmail.com>
2023-07-17 15:18:40 -0700

    dfghjkl

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,6 @@
 
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
 */
 int main(void)
@@ -21,7 +20,7 @@ else if ((n % 10) < 6 && (n % 10) != 0)
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 }
 else
-}
+{
 printf("Last digit of %d is %d and is 0\n", n, n % 10);
 }
 return (0);
