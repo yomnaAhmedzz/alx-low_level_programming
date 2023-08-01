15c0a90caa43297a0afe8c12d8e449c3e1f2c2cc
root <ibrahimyomna376@gmail.com>
2023-07-31 17:04:28 -0700

    2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,21 +1,21 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
  */
-int main(void)
+char *_strchr(char *s, char c)
 {
-    char *s = "hello";
-    char *f;
+	int i;
 
-    f = _strchr(s, 'l');
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
 
-    if (f != NULL)
-    {
-        printf("%s\n", f);
-    }
-    return (0);
+	return (NULL);
 }
