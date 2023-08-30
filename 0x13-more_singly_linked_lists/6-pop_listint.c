de5f2a4b7b04ce365819278c48cd09528f4aa37d
yomnaAhmedzz <yomnaahmedd5522@gmail.com>
2023-08-30 14:13:17 -0700

    23423

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * pop_listint - to pop list
+ * @head: head pointer
+ * Return: return the data, or 0 if no data 
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int num;
+
+	if (!head || !*head)
+		return (0);
+
+	num = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+
+	return (num);
+}
+
