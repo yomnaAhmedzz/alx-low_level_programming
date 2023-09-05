060c5ee204f2e7f46d028fbe84a07be3b5f4e2c4
yomnaAhmedzz <yomnaahmedd5522@gmail.com>
2023-09-04 17:16:14 -0700

    jhgf

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,101 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create_buffer(char *file);
+void close_file(int fd);
+
+/**
+ * create_buffer - okey.
+ * @file: 3-cp.c.
+ *
+ * Return:3-cp.c.
+ */
+char *create_buffer(char *file)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buffer);
+}
+
+/**
+ * close_file - Closes file descriptors.
+ * @d: close.
+ */
+void close_file(int d)
+{
+	int c;
+
+	c = close(d);
+
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close d %d\n", d);
+		exit(100);
+	}
+}
+
+/**
+ * main - file.
+ * @argc: The number.
+ * @argv: An array.
+ *
+ * Return:  success.
+ *
+ * Description: exit code 100.
+ */
+int main(int argc, char *argv[])
+{
+	int from, to, r, yy;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buffer = create_buffer(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (from == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+
+		yy= write(to, buffer, r);
+		if (to == -1 || yy== -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (r > 0);
+
+	free(buffer);
+	close_file(from);
+	close_file(to);
+
+	return (0);
+}
+
